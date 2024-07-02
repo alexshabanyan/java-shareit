0f248267f8253e1bef68fe66a1a13f2305be9b1e
alexander <shabanyan94@yandex.ru>
2024-07-02 05:54:23 +0300

    fix: ТЗ-16

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 
-import javax.validation.Valid;
-
 @Controller
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
@@ -17,7 +15,7 @@ public class UserController {
     private final UserClient userClient;
 
     @PostMapping
-    public ResponseEntity<Object> createUser(@Valid @RequestBody UserDto userDto) {
+    public ResponseEntity<Object> createUser(@RequestBody UserDto userDto) {
         log.info("Создание пользователя userDto={}", userDto);
         return userClient.createUser(userDto);
     }
@@ -35,7 +33,7 @@ public class UserController {
     }
 
     @PatchMapping("/{userId}")
-    public ResponseEntity<Object> updateUser(@PathVariable Long userId, @Valid @RequestBody UserDto userDto) {
+    public ResponseEntity<Object> updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
         log.info("Обновление пользователя userDto={}", userDto);
         return userClient.updateUser(userId, userDto);
     }
